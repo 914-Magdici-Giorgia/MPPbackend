1c6691a6c9ef35f262e0c307cb7a9540d942739d
Giorgia Magdici <giorgia.magdici@stud.ubbcluj.ro>
2024-05-30 22:20:03 +0300

    gata ma odata + 1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT ||5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-sequelize.sync({force:false});
+sequelize.sync({force:true});
 // Mount the millsRoute middleware
 app.use("/api", millsRoute);
 
